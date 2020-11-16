c46b93ff666f1ad302fb9ca028dd0fb71da86768
hamiltonbueno2210 <74570864+hamiltonbueno2210@users.noreply.github.com>
2020-11-16 19:38:48 -0300

    aqui coloco uma mensagem, tipo: MeuProjeto

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+$('.slider-principal').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
+
